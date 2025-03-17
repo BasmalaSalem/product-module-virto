8cdb2dbc3119ebc928ff19a3b120e5c9f279da3a
RadwaaAyman <radwa.ayman@totplatform.com>
2025-03-17 13:49:54 +0200

    add web project thta contain module class , add manifest file  that contain data of module

---
+++ ProductPOCModule/BaseProductModule/src/BaseProductModule.Web/Controllers/Api/BaseProductController.cs
@@ -0,0 +1,49 @@
+﻿using BaseProductModule.Core.Model;
+using BaseProductModule.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProductModule.Web.Controllers.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseProductController : ControllerBase
+{
+    private readonly IBaseProductService _productService;
+    public BaseProductController(IBaseProductService productService)
+    {
+        _productService = productService;
+
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProductById(string id)
+    {
+        return await _productService.GetByIdAsync(id);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetAllProducts()
+    {
+        return await _productService.GetAllAsync();
+    }
+    [HttpPost]
+    public async Task<ActionResult<Product>> AddProduct(Product product)
+    {
+        return await _productService.CreateAsync(product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Product>> UpdateProduct(string id, Product product)
+    {
+        return await _productService.UpdateAsync(id, product);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        await _productService.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
+
--- ProductPOCModule/BaseProductModule/src/BaseProductModule.Web/Module.cs
+++ ProductPOCModule/BaseProductModule/src/BaseProductModule.Web/Module.cs
@@ -1,6 +1,44 @@
-﻿namespace BaseProductModule.Web
+﻿using BaseProductModule.Core.Services;
+using BaseProductModule.Data.Repositories;
+using BaseProductModule.Data.Services;
+using VirtoCommerce.Platform.Data.SqlServer.Extensions;
+using VirtoCommerce.Platform.Core.Modularity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProductModule.Web;
+
+public class Module : IModule , IHasConfiguration
 {
-    public class Module
+    public ManifestModuleInfo ModuleInfo { get ; set ; }
+    public IConfiguration Configuration { get ; set ; }
+
+    public void Initialize(IServiceCollection serviceCollection)
     {
+        var connectionString = Configuration.GetConnectionString(ModuleInfo.Id) ?? Configuration.GetConnectionString("VirtoCommerce");
+
+        serviceCollection.AddDbContext<BaseProductDbContext>(options =>
+            options.UseSqlServerDatabase(connectionString, typeof(SqlServerDataAssemblyMarker), Configuration));
+
+        serviceCollection.AddTransient<IProductRepository, ProductRepository>();
+
+        // Register services
+        serviceCollection.AddTransient<IBaseProductService, BaseProductService>();
+    }
+
+    public void PostInitialize(IApplicationBuilder appBuilder)
+    {
+        var serviceProvider = appBuilder.ApplicationServices;
+
+        // Apply migrations
+        using var serviceScope = serviceProvider.CreateScope();
+       
+        using var dbContext = serviceScope.ServiceProvider.GetRequiredService<BaseProductDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+    public void Uninstall()
+    {
+        throw new NotImplementedException();
     }
 }
