bd064fc67bb5eff44250eb940ebaa5e242749dea
RadwaaAyman <radwa.ayman@totplatform.com>
2025-03-18 13:24:48 +0200

    solve sql server dbcontext factory

--- ProductPOCModule/PhysicalProductModule/src/PhysicalProductModule.Data/Repositories/SqlServerDbContextFactory.cs
+++ ProductPOCModule/PhysicalProductModule/src/PhysicalProductModule.Data/Repositories/SqlServerDbContextFactory.cs
@@ -9,18 +9,18 @@ using System.Threading.Tasks;
 
 namespace PhysicalProductModule.Data.Repositories;
 
-public class SqlServerDbContextFactory : IDesignTimeDbContextFactory<PhysicalProductDbContext>
-{
-    public PhysicalProductDbContext CreateDbContext(string[] args)
-    {
-        var builder = new DbContextOptionsBuilder<PhysicalProductDbContext>();
-        var connectionString = args.Length != 0 ? args[0] : "Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=sa;Password=123;Connect Timeout=30;TrustServerCertificate=True;";
+//public class SqlServerDbContextFactory : IDesignTimeDbContextFactory<PhysicalProductDbContext>
+//{
+//    public PhysicalProductDbContext CreateDbContext(string[] args)
+//    {
+//        var builder = new DbContextOptionsBuilder<PhysicalProductDbContext>();
+//        var connectionString = args.Length != 0 ? args[0] : "Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=sa;Password=123;Connect Timeout=30;TrustServerCertificate=True;";
 
-        builder.UseSqlServer(
-            connectionString,
-            db => db.MigrationsAssembly(typeof(SqlServerDataAssemblyMarker).Assembly.GetName().Name));
+//        builder.UseSqlServer(
+//            connectionString,
+//            db => db.MigrationsAssembly(typeof(SqlServerDataAssemblyMarker).Assembly.GetName().Name));
 
 
-        return new PhysicalProductDbContext(builder.Options);
-    }
-}
+//        return new PhysicalProductDbContext(builder.Options);
+//    }
+//}
