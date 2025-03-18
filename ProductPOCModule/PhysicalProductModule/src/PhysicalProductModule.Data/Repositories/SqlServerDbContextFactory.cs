bfa78355cadacbdba5335c29fde486106770424b
RadwaaAyman <radwa.ayman@totplatform.com~>
2025-03-18 05:42:38 +0200

    add dbcontext in physical product module

---
+++ ProductPOCModule/PhysicalProductModule/src/PhysicalProductModule.Data/Repositories/SqlServerDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using BaseProductModule.Data.Repositories;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicalProductModule.Data.Repositories;
+
+public class SqlServerDbContextFactory : IDesignTimeDbContextFactory<PhysicalProductDbContext>
+{
+    public PhysicalProductDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<PhysicalProductDbContext>();
+        var connectionString = args.Length != 0 ? args[0] : "Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=sa;Password=123;Connect Timeout=30;TrustServerCertificate=True;";
+
+        builder.UseSqlServer(
+            connectionString,
+            db => db.MigrationsAssembly(typeof(SqlServerDataAssemblyMarker).Assembly.GetName().Name));
+
+
+        return new PhysicalProductDbContext(builder.Options);
+    }
+}
---
+++ ProductPOCModule/PhysicalProductModule/src/PhysicalProductModule.Web/Module.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhysicalProductModule.Data.Repositories;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Data.SqlServer.Extensions;
+
+namespace PhysicalProductModule.Web;
+
+public class Module : IModule , IHasConfiguration
+{
+    public ManifestModuleInfo ModuleInfo { get ; set ; }
+    public IConfiguration Configuration { get ; set ; }
+
+    public void Initialize(IServiceCollection serviceCollection)
+    {
+        var connectionString = Configuration.GetConnectionString(ModuleInfo.Id) ?? Configuration.GetConnectionString("VirtoCommerce");
+
+        serviceCollection.AddDbContext<PhysicalProductDbContext>(options =>
+            options.UseSqlServerDatabase(connectionString, typeof(SqlServerDataAssemblyMarker), Configuration));
+    }
+
+    public void PostInitialize(IApplicationBuilder appBuilder)
+    {
+        var serviceProvider = appBuilder.ApplicationServices;
+
+        // Apply migrations
+        using var serviceScope = serviceProvider.CreateScope();
+
+        using var dbContext = serviceScope.ServiceProvider.GetRequiredService<PhysicalProductDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+    public void Uninstall()
+    {
+        throw new NotImplementedException();
+    }
+}
